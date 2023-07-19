46526d94b28f3a0fa83c9636a6a0bd2d0163f641
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-07-19 11:54:12 -0500

    Init Pixi dependencies and example

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useMemo } from "react";
 import "./App.css";
+import { BlurFilter } from "pixi.js";
+import { Stage, Sprite, Container, Text } from "@pixi/react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const blurFilter = useMemo(() => new BlurFilter(4), []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stage>
+        <Sprite
+          image="https://pixijs.io/pixi-react/img/bunny.png"
+          x={400}
+          y={270}
+          anchor={{ x: 0.5, y: 0.5 }}
+        />
+
+        <Container x={400} y={330}>
+          <Text
+            text="Hello World"
+            anchor={{ x: 0.5, y: 0.5 }}
+            filters={[blurFilter]}
+          />
+        </Container>
+      </Stage>
     </>
   );
 }
