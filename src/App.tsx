36e07f59ff403a19c66081660dc41cfd37cc76c8
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-07-19 15:03:55 -0500

    Revert to clean slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useMemo } from "react";
 import "./App.css";
-import { BlurFilter } from "pixi.js";
-import { Stage, Sprite, Container, Text } from "@pixi/react";
 
 function App() {
-  const blurFilter = useMemo(() => new BlurFilter(4), []);
+  console.log(window.innerWidth, window.innerHeight);
 
-  return (
-    <>
-      <Stage>
-        <Sprite
-          image="https://pixijs.io/pixi-react/img/bunny.png"
-          x={400}
-          y={270}
-          anchor={{ x: 0.5, y: 0.5 }}
-        />
-
-        <Container x={400} y={330}>
-          <Text
-            text="Hello World"
-            anchor={{ x: 0.5, y: 0.5 }}
-            filters={[blurFilter]}
-          />
-        </Container>
-      </Stage>
-    </>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
